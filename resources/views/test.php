c8f30e08fe4dd1e2b762aba0dbc770dccfec65a2
wngur6076 <wngur6076@naver.com>
2021-06-03 10:47:55 +0900

    Laravel Url, URL to Route

---
+++ resources/views/test.php
@@ -0,0 +1,7 @@
+<?php
+    $world = "Hi Ariyan";
+    echo $world;
+
+?>
+
+{{ $world }}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    echo "This is ";
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', [ContactController::class, 'index'])->name('con');
