3f0a1fc7646684bf6e6ea00cb12e1ca410e25871
wngur6076 <wngur6076@naver.com>
2021-06-10 22:07:27 +0900

    카테고리 업데이트

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,12 +13,12 @@ class CategoryController extends Controller
 {
     public function AllCat()
     {
-        $categories = DB::table('categories')
+        /* $categories = DB::table('categories')
             ->join('users', 'categories.user_id', 'users.id')
             ->select('categories.*', 'users.name')
-            ->latest()->paginate(5);
+            ->latest()->paginate(5); */
 
-        // $categories = Category::latest()->paginate(5);
+        $categories = Category::latest()->paginate(5);
         // $categories = DB::table('categories')->latest()->paginate(5);
         return view('admin.category.index', compact('categories'));
     }
@@ -44,5 +44,22 @@ class CategoryController extends Controller
         DB::table('categories')->insert($data);
 
         return redirect()->back()->with('success', 'Category Inserted Successfull');
+    }
+
+    public function Edit($id)
+    {
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function Update(Request $request, $id)
+    {
+        $update = Category::find($id)->update([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return redirect()->route('all.category')->with('success', 'Category Updated Successfull');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.ca
 
 Route::post('/category/add', [CategoryController::class, 'AddCat'])->name('store.category');
 
+Route::get('/category/edit/{id}', [CategoryController::class, 'Edit']);
+Route::post('/category/update/{id}', [CategoryController::class, 'Update']);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
     // $users = User::all();
