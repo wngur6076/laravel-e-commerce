75fd69efa49a3290da64b84784ed4ebedccaa060
wngur6076 <wngur6076@naver.com>
2021-06-10 13:04:49 +0900

    Eloquent ORM Read Data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use Carbon\Carbon;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 
@@ -12,7 +13,8 @@ class CategoryController extends Controller
 {
     public function AllCat()
     {
-        return view('admin.category.index');
+        $categories = Category::latest()->get();
+        return view('admin.category.index', compact('categories'));
     }
 
     public function AddCat(Request $request)
@@ -25,10 +27,15 @@ class CategoryController extends Controller
             'category_name.max' => 'Category Less Then 255Chars',
         ]);
 
-        Category::create([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-        ]);
+        // Category::create([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        // ]);
+
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        DB::table('categories')->insert($data);
 
         return redirect()->back()->with('success', 'Category Inserted Successfull');
     }
