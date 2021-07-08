a50acd3a6cf3fc843aad49f62f88162346add4b7
wngur6076 <wngur6076@naver.com>
2021-07-08 21:13:54 +0900

    카테고리 수정

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -33,7 +33,29 @@ class CategoryController extends Controller
             'category_name.max' => 'Category Less Then 255Chars',
         ]);
 
-        // Category::create([
+        Category::create([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id'] = Auth::user()->id;
+        // DB::table('categories')->insert($data);
+
+        return redirect()->back()->with('success', 'Category Inserted Successfull');
+    }
+
+    public function Edit($id)
+    {
+        // $category = Category::find($id);
+        $category = DB::table('categories')->where('id', $id)->first();
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function Update(Request $request, $id)
+    {
+        // $update = Category::find($id)->update([
         //     'category_name' => $request->category_name,
         //     'user_id' => Auth::user()->id,
         // ]);
@@ -41,23 +63,8 @@ class CategoryController extends Controller
         $data = array();
         $data['category_name'] = $request->category_name;
         $data['user_id'] = Auth::user()->id;
-        DB::table('categories')->insert($data);
 
-        return redirect()->back()->with('success', 'Category Inserted Successfull');
-    }
-
-    public function Edit($id)
-    {
-        $category = Category::find($id);
-        return view('admin.category.edit', compact('category'));
-    }
-
-    public function Update(Request $request, $id)
-    {
-        $update = Category::find($id)->update([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-        ]);
+        DB::table('categories')->where('id', $id)->update($data);
 
         return redirect()->route('all.category')->with('success', 'Category Updated Successfull');
 
--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        ProtoneMedia\LaravelFFMpeg\Support\ServiceProvider::class,
 
     ],
 
@@ -229,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'FFMpeg' => ProtoneMedia\LaravelFFMpeg\Support\FFMpeg::class
     ],
 
 ];
